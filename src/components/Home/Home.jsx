c6dcaaea7f5c8013ee5fd321dbf446c6bb21ea98
Seb Davies <seb_davies@hotmail.co.uk>
2024-07-10 11:30:22 +0100

    nearly finish the home view

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Intro from "../Intro/Intro";
 import "../../assets/css/home.css";
+import { NavLink } from "react-router-dom";
 function Home() {
   return (
     <div className="homeWrapper">
@@ -9,17 +10,18 @@ function Home() {
         <h3> About Me</h3>
       </div>
       <div className="homeSelfie"> </div>
-      <div className="homeBioBody">
+      <div className="homeBioBlack">
         <p>
-          Hi there! My name is Sebastian Davies and I'm an ethusiastic
+          Hi there! My name is Sebastian Davies and I'm an enthusiastic
           full-stack developer based in Brighton. I honed both my frontend &
           backend development aptitude during my time at the TechNativeDigital
           full-stack bootcamp. My original background is in theoretical physics
           academia, allowing me to bring a unique and inquisitive approach to
-          software development.
+          software development. I am veraciously curious and love learning.
         </p>
       </div>
-      <div className="homeBioSkills">
+      <img className="homePic" src="./presenting.jpg" />
+      <div className="homeBioGrey">
         <p>
           <strong>Technical Skills:</strong> During the TechNative Academy
           bootcamp I developed a vast skillset of technical coding ability,
@@ -30,8 +32,68 @@ function Home() {
           <strong>Git</strong> & <strong>GitHub</strong>, <strong>Mocha</strong>
           and <strong>Chai</strong>. Whilst having also picked up technical
           skills prior to the bootcamp in C++ and Python libraries such as{" "}
-          <strong>Pandas</strong> and <strong>SciKitLearn</strong>
+          <strong>Pandas</strong> and <strong>SciKitLearn</strong>.
         </p>
+      </div>
+      <img src="./project1pic.jpg" className="homePic" />
+      <div className="homeBioBlack">
+        <p>
+          <strong>Hands On Experience</strong>: Throughout the TechNativeDigital
+          Bootcamp I have had real world exposure to delivering group projects
+          from start to completion. Projects were delivered within a tight
+          deadline and to a client brief and culminated in presentations to
+          industry professionals. You can find out more about these projects{" "}
+          <NavLink className="homeBioNav" key={"/projects"} to={"/projects"}>
+            here
+          </NavLink>
+          .
+        </p>
+      </div>
+      <img src="./technativegroup.jpg" className="homePic" />
+      <div className="homeBioGrey">
+        <p>
+          <strong>Exposure To Industry Experts</strong>: Each week during the
+          bootcamp I attended Q&A style talks from industry experts, not only in
+          web-development but also in surrounding fields (project management,
+          design etc). I also enjoy attending local Brighton tech events, such
+          as Async and TechJumpStart. This exposure has given me an insight into
+          the real world of development and has only made me more excited to
+          jump into that first role!
+        </p>
+      </div>
+      <img src="./arduinoday.jpg" className="homePic" />
+      <div className="homeBioBlack">
+        <p>
+          <strong>Teamwork</strong>: The bootcamp emphasised a strong culture of
+          teamwork and pair-programming and AGILE methodology. Honestly, this
+          was frightening at first, however, it is now one of my favourite
+          aspects of development. Working closely with others has allowed me to
+          grow, share ideas, tech tips and improve via code reviews. Group work
+          has also given me the ability to thrive within the workplace,
+          contributing and benefiting from shared discussion and knowledge.
+        </p>
+      </div>
+      <img src="./finaldaybeach.jpg" className="homePic" />
+      <div className="homeBioGrey">
+        <p>
+          <strong>Problem Solving</strong>: My background in theoretical physics
+          and healthcare service has equipped me with excellent logical,
+          mathematical and social problem solving skills. Whilst, the bootcamp
+          has nourished this existing skillset I have also developed strong
+          technical and cross-functional skills allowing me to slot into a
+          developer role and quickly become effective.
+        </p>
+      </div>
+      <img src="./meetingpic.png" className="homePic" />
+      <div className="homeBioBlack">
+        <h4 className="homeBioContactHeading">Get In Touch</h4>
+        <p>
+          I am eager to hear from you, whether you have any questions or
+          comments please feel free to get in touch.
+        </p>
+        <button className="homeBioContactButton">
+          <strong>CONTACT</strong>
+        </button>
       </div>
     </div>
   );
